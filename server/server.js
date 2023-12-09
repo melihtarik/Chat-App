0077282e5da9d46fbe779fb6112b5da2fd454a49
melihtarik <muhammet.tarik@gmail.com>
2023-12-09 19:08:54 +0300

    Commit

--- server/server.js
+++ server/server.js
@@ -1,15 +1,31 @@
 const path = require("path");
 const http = require("http");
 const express = require("express");
-const socket = require("socket.io");
+const socketIO = require("socket.io");
 
 const publicPath = path.join(__dirname, "/../public");
 const port = process.env.PORT || 3000;
 let app = express();
 let server = http.createServer(app);
-let oi = socketIO(server);
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
+
+io.on("connection", (socket) => {
+  console.log(
+    "Yeni Kullanıcı Bağlandı..\nKullanıcı ID: ",
+    socket.id,
+    "\n--------------------------------------------"
+  );
+
+  socket.on("disconnect", () => {
+    console.log(
+      "Kullanıcı ID: ",
+      socket.id,
+      " Bağlantısı Kesildi..\n--------------------------------------------"
+    );
+  });
+});
 
 server.listen(3000, () => {
   console.log(`Server Çalışıyor Port: ${port}`);
